5e619644704de40485617651127bb051fd8336d8
Alexandre 'Loreilei' Poirot <alexandre.poirot@gmail.com>
2022-11-01 02:58:42 +0100

    Refactor match handling Option and Result in analysis module
    + Make use of ok_or and map_err

--- src/analysis.rs
+++ src/analysis.rs
@@ -6,6 +6,19 @@ pub enum AnalysisError {
     MetadataAnalysisError(metadata::Error),
     DataAnalysisError(data::Error),
 }
+
+impl AnalysisError {
+    pub fn header(err: header::Error) -> Self {
+        AnalysisError::HeaderAnalysisError(err)
+    }
+    pub fn metadata(err: metadata::Error) -> Self {
+        AnalysisError::MetadataAnalysisError(err)
+    }
+    pub fn data(err: data::Error) -> Self {
+        AnalysisError::DataAnalysisError(err)
+    }
+}
+
 pub mod data {
     use std::ops::Range;
 
@@ -85,19 +98,11 @@ pub mod header {
         }
 
         pub fn from_data(data: &[u8]) -> Result<Self, Error> {
-            let tag = match data.get(TAG_RANGE) {
-                Some(data) => data.iter().map(|d| *d as char).collect::<String>(),
-                None => {
-                    return Err(Error::InvalidLength);
-                }
-            };
+            let tag_data = data.get(TAG_RANGE).ok_or(Error::InvalidLength)?;
+            let version_data = data.get(VERSION_RANGE).ok_or(Error::InvalidLength)?;
 
-            let version = match data.get(VERSION_RANGE) {
-                Some(data) => as_u32(data),
-                None => {
-                    return Err(Error::InvalidLength);
-                }
-            };
+            let tag = tag_data.iter().map(|d| *d as char).collect::<String>();
+            let version = as_u32(version_data);
 
             if tag != VALID_TAG {
                 return Err(Error::InvalidTag); // Assuming we're dealing with little endian documents
@@ -136,23 +141,21 @@ pub mod metadata {
 
     impl ContainerBase {
         pub fn from_data(data: &[u8]) -> Result<Self, Error> {
-            let size = match data.get(SIZE_FIELD_RANGE) {
-                Some(data) => as_u32(data),
-                None => return Err(Error::InvalidContainerBaseLength),
-            };
+            let size_data = data
+                .get(SIZE_FIELD_RANGE)
+                .ok_or(Error::InvalidContainerBaseLength)?;
+            let object_flag_and_length_data = data
+                .get(OBJECT_FLAG_AND_LENGTH_RANGE)
+                .ok_or(Error::InvalidContainerBaseLength)?;
+            let table_offset_data = data
+                .get(TABLE_OFFSET_RANGE)
+                .ok_or(Error::InvalidContainerBaseLength)?;
 
-            let object_flag_and_length = match data.get(OBJECT_FLAG_AND_LENGTH_RANGE) {
-                Some(data) => as_u32(data),
-                None => return Err(Error::InvalidContainerBaseLength),
-            };
-
+            let size = as_u32(size_data);
+            let object_flag_and_length = as_u32(object_flag_and_length_data);
             let is_object = (object_flag_and_length & 0b1) != 0;
             let length = (object_flag_and_length & !0b1_u32) >> 1;
-
-            let table_offset = match data.get(TABLE_OFFSET_RANGE) {
-                Some(data) => as_u32(data),
-                None => return Err(Error::InvalidContainerBaseLength),
-            };
+            let table_offset = as_u32(table_offset_data);
 
             Ok(ContainerBase {
                 size,
@@ -180,21 +183,15 @@ pub mod metadata {
 
     impl ValueHeader {
         pub fn from_data(data: &[u8], position: usize) -> Result<Self, Error> {
-            let header = match data.first() {
-                Some(header) => header,
-                None => {
-                    return Err(Error::InvalidValueHeaderSize);
-                }
-            };
+            let header = data.first().ok_or(Error::InvalidValueHeaderSize)?;
+            let value_bit_field_data = data
+                .get(VALUE_BIT_FIELD_RANGE)
+                .ok_or(Error::InvalidValueHeaderSize)?;
+
             let qt_value_type = header & QT_VALUE_TYPE_MASK;
             let latin_or_int_value_flag = (header & LATIN_OR_INT_VALUE_FLAG_MASK) != 0;
             let latin_key_flag = (header & LATIN_KEY_FLAG_MASK) != 0;
-            let value_bit_field = match data.get(VALUE_BIT_FIELD_RANGE) {
-                Some(data) => as_u27(as_u32(data)),
-                None => {
-                    return Err(Error::InvalidValueHeaderSize);
-                }
-            };
+            let value_bit_field = as_u27(as_u32(value_bit_field_data));
 
             Ok(ValueHeader {
                 qt_value_type,
@@ -213,148 +210,114 @@ pub mod metadata {
 }
 
 pub fn analyze_document(data: &[u8]) -> Result<data::Value, AnalysisError> {
-    let header = match data.get(0..header::HEADER_LENGTH) {
-        Some(data) => header::QbjsHeader::from_data(data),
-        None => Err(header::Error::InvalidLength),
-    };
+    let header_data = data
+        .get(0..header::HEADER_LENGTH)
+        .ok_or(header::Error::InvalidLength)
+        .map_err(AnalysisError::header)?;
 
-    match header {
-        Ok(_) => {}
-        Err(e) => {
-            return Err(AnalysisError::HeaderAnalysisError(e));
-        }
-    }
+    header::QbjsHeader::from_data(header_data).map_err(AnalysisError::header)?;
 
     let container_base_range =
         header::HEADER_LENGTH..(header::HEADER_LENGTH + metadata::CONTAINER_BASE_LENGTH);
-    let container_base = match data.get(container_base_range) {
-        Some(data) => metadata::ContainerBase::from_data(data),
-        None => Err(metadata::Error::InvalidContainerBaseLength),
+
+    let container_base_data = data
+        .get(container_base_range)
+        .ok_or(metadata::Error::InvalidContainerBaseLength)
+        .map_err(AnalysisError::metadata)?;
+
+    let container_base =
+        metadata::ContainerBase::from_data(container_base_data).map_err(AnalysisError::metadata)?;
+
+    let analyze_container = if container_base.is_object {
+        analyze_object
+    } else {
+        analyze_array
     };
 
-    match container_base {
-        Ok(container_base) => {
-            if container_base.is_object {
-                match analyze_object(data, header::HEADER_LENGTH) {
-                    Ok(analyzed_object) => Ok(analyzed_object.0),
-                    Err(e) => Err(e),
-                }
-            } else {
-                match analyze_array(data, header::HEADER_LENGTH) {
-                    Ok(analyzed_array) => Ok(analyzed_array.0),
-                    Err(e) => Err(e),
-                }
-            }
-        }
-        Err(e) => Err(AnalysisError::MetadataAnalysisError(e)),
-    }
+    analyze_container(data, header::HEADER_LENGTH).map(|(value, _)| value)
 }
 
 fn analyze_array(data: &[u8], base_start: usize) -> Result<(data::Value, usize), AnalysisError> {
     let base_end = base_start + metadata::CONTAINER_BASE_LENGTH;
     let base_range = base_start..base_end;
 
-    let array_info = match data.get(base_range) {
-        Some(data) => metadata::ContainerBase::from_data(data),
-        None => Err(metadata::Error::InvalidContainerBaseLength),
-    };
+    let array_info_data = data
+        .get(base_range)
+        .ok_or(metadata::Error::InvalidContainerBaseLength)
+        .map_err(AnalysisError::metadata)?;
 
-    match array_info {
-        Ok(array_info) => {
-            if array_info.is_object {
-                return Err(AnalysisError::DataAnalysisError(
-                    data::Error::InvalidArrayContainer,
-                ));
-            }
+    let array_info =
+        metadata::ContainerBase::from_data(array_info_data).map_err(AnalysisError::metadata)?;
 
-            let nb_values = array_info.length as usize;
-
-            let mut values = Vec::<data::Value>::new();
-            values.reserve_exact(nb_values);
-
-            let mut offset = base_start + array_info.table_offset as usize;
-            for _i in 0..nb_values {
-                let header_start = offset;
-                let header_end = header_start + metadata::VALUE_HEADER_BYTE_SIZE;
-
-                let header = match data.get(header_start..header_end) {
-                    Some(data) => metadata::ValueHeader::from_data(data, header_start),
-                    None => {
-                        return Err(AnalysisError::MetadataAnalysisError(
-                            metadata::Error::InvalidValueHeaderSize,
-                        ));
-                    }
-                };
-
-                match header {
-                    Ok(header) => match analyze_value(data, &header, base_start) {
-                        Ok((value, _)) => {
-                            values.push(value);
-
-                            offset = header_end;
-                        }
-                        Err(e) => {
-                            return Err(e);
-                        }
-                    },
-                    Err(e) => {
-                        return Err(AnalysisError::MetadataAnalysisError(e));
-                    }
-                }
-            }
-
-            Ok((
-                data::Value::Array(data::Array { values }),
-                base_start + array_info.size as usize,
-            ))
-        }
-        Err(e) => Err(AnalysisError::MetadataAnalysisError(e)),
+    if array_info.is_object {
+        return Err(AnalysisError::data(data::Error::InvalidArrayContainer));
     }
+
+    let nb_values = array_info.length as usize;
+
+    let mut values = Vec::<data::Value>::new();
+    values.reserve_exact(nb_values);
+
+    let mut offset = base_start + array_info.table_offset as usize;
+    for _i in 0..nb_values {
+        let header_start = offset;
+        let header_end = header_start + metadata::VALUE_HEADER_BYTE_SIZE;
+
+        let header_data = data
+            .get(header_start..header_end)
+            .ok_or(metadata::Error::InvalidValueHeaderSize)
+            .map_err(AnalysisError::metadata)?;
+
+        let header = metadata::ValueHeader::from_data(header_data, header_start)
+            .map_err(AnalysisError::metadata)?;
+
+        let (value, _) = analyze_value(data, &header, base_start)?;
+
+        values.push(value);
+
+        offset = header_end;
+    }
+
+    Ok((
+        data::Value::Array(data::Array { values }),
+        base_start + array_info.size as usize,
+    ))
 }
 
 fn analyze_object(data: &[u8], base_start: usize) -> Result<(data::Value, usize), AnalysisError> {
     let base_end = base_start + metadata::CONTAINER_BASE_LENGTH;
     let base_range = base_start..base_end;
 
-    let object_info = match data.get(base_range) {
-        Some(data) => metadata::ContainerBase::from_data(data),
-        None => Err(metadata::Error::InvalidContainerBaseLength),
-    };
+    let object_info_data = data
+        .get(base_range)
+        .ok_or(metadata::Error::InvalidContainerBaseLength)
+        .map_err(AnalysisError::metadata)?;
 
-    match object_info {
-        Ok(object_info) => {
-            if !object_info.is_object {
-                return Err(AnalysisError::DataAnalysisError(
-                    data::Error::InvalidObjectContainer,
-                ));
-            }
+    let object_info =
+        metadata::ContainerBase::from_data(object_info_data).map_err(AnalysisError::metadata)?;
 
-            let nb_entries = object_info.length as usize;
-
-            let mut entries = Vec::<data::Entry>::new();
-            entries.reserve_exact(nb_entries);
-
-            let mut offset = base_end;
-            for _i in 0..nb_entries {
-                match analyze_entry(data, offset, base_start) {
-                    Ok((entry, entry_end)) => {
-                        entries.push(entry);
-
-                        offset = entry_end;
-                    }
-                    Err(e) => {
-                        return Err(e);
-                    }
-                }
-            }
-
-            Ok((
-                data::Value::Object(data::Object { entries }),
-                base_start + object_info.size as usize,
-            ))
-        }
-        Err(e) => Err(AnalysisError::MetadataAnalysisError(e)),
+    if !object_info.is_object {
+        return Err(AnalysisError::data(data::Error::InvalidObjectContainer));
     }
+
+    let nb_entries = object_info.length as usize;
+
+    let mut entries = Vec::<data::Entry>::new();
+    entries.reserve_exact(nb_entries);
+
+    let mut offset = base_end;
+    for _i in 0..nb_entries {
+        let (entry, entry_end) = analyze_entry(data, offset, base_start)?;
+
+        entries.push(entry);
+
+        offset = entry_end;
+    }
+
+    Ok((
+        data::Value::Object(data::Object { entries }),
+        base_start + object_info.size as usize,
+    ))
 }
 
 fn analyze_entry(
@@ -365,46 +328,32 @@ fn analyze_entry(
     let header_end = entry_start + metadata::VALUE_HEADER_BYTE_SIZE;
     let header_range = entry_start..header_end;
 
-    let header_data = match data.get(header_range) {
-        Some(data) => data,
-        None => {
-            return Err(AnalysisError::MetadataAnalysisError(
-                metadata::Error::InvalidValueHeaderSize,
-            ));
-        }
+    let header_data = data
+        .get(header_range)
+        .ok_or(metadata::Error::InvalidValueHeaderSize)
+        .map_err(AnalysisError::metadata)?;
+
+    let header = metadata::ValueHeader::from_data(header_data, entry_start)
+        .map_err(AnalysisError::metadata)?;
+
+    let analyze_key = if header.latin_key_flag {
+        analyze_latin1_key
+    } else {
+        analyze_utf16_key
     };
 
-    match metadata::ValueHeader::from_data(header_data, entry_start) {
-        Ok(header) => {
-            let key_analysis_result = if header.latin_key_flag {
-                analyze_latin1_key(data, header_end)
-            } else {
-                analyze_utf16_key(data, header_end)
-            };
+    let (key, key_end) = analyze_key(data, header_end)?;
 
-            let (key, key_end) = match key_analysis_result {
-                Ok(key_info) => key_info,
-                Err(e) => {
-                    return Err(e);
-                }
-            };
+    let (value, value_end) = analyze_value(data, &header, object_start)?;
 
-            match analyze_value(data, &header, object_start) {
-                Ok((value, value_end)) => {
-                    let entry_end = match value {
-                        data::Value::Null(_)
-                        | data::Value::Bool(_)
-                        | data::Value::SelfContainedNumber(_) => key_end,
-                        _ => value_end,
-                    };
-
-                    Ok((data::Entry { key, value }, entry_end))
-                }
-                Err(e) => Err(e),
-            }
+    let entry_end = match value {
+        data::Value::Null(_) | data::Value::Bool(_) | data::Value::SelfContainedNumber(_) => {
+            key_end
         }
-        Err(e) => Err(AnalysisError::MetadataAnalysisError(e)),
-    }
+        _ => value_end,
+    };
+
+    Ok((data::Entry { key, value }, entry_end))
 }
 
 fn analyze_latin1_string(data: &[u8], string_field_start: usize) -> (data::ByteField, usize) {
@@ -447,28 +396,25 @@ fn analyze_utf16_string(data: &[u8], string_field_start: usize) -> (data::ByteFi
 
 fn analyze_latin1_key(data: &[u8], key_start: usize) -> Result<(data::Key, usize), AnalysisError> {
     let size_field_range = key_start..(key_start + metadata::LATIN1_SIZE_FIELD_LENGTH);
-    match data.get(size_field_range) {
-        Some(data) => {
-            let (bytefield, key_end) = analyze_latin1_string(data, key_start);
-            Ok((data::Key::Latin1String(bytefield), key_end))
-        }
-        None => Err(AnalysisError::DataAnalysisError(
-            data::Error::InvalidValueLength,
-        )),
-    }
+    let key_data = data
+        .get(size_field_range)
+        .ok_or(data::Error::InvalidValueLength)
+        .map_err(AnalysisError::data)?;
+
+    let (bytefield, key_end) = analyze_latin1_string(key_data, key_start);
+    Ok((data::Key::Latin1String(bytefield), key_end))
 }
 
 fn analyze_utf16_key(data: &[u8], key_start: usize) -> Result<(data::Key, usize), AnalysisError> {
     let size_field_range = key_start..(key_start + metadata::UTF16_SIZE_FIELD_LENGTH);
-    match data.get(size_field_range) {
-        Some(data) => {
-            let (bytefield, key_end) = analyze_utf16_string(data, key_start);
-            Ok((data::Key::Utf16String(bytefield), key_end))
-        }
-        None => Err(AnalysisError::DataAnalysisError(
-            data::Error::InvalidValueLength,
-        )),
-    }
+
+    let key_data = data
+        .get(size_field_range)
+        .ok_or(data::Error::InvalidValueLength)
+        .map_err(AnalysisError::data)?;
+
+    let (bytefield, key_end) = analyze_utf16_string(key_data, key_start);
+    Ok((data::Key::Utf16String(bytefield), key_end))
 }
 
 const QT_NULL_VALUE: u8 = 0;
@@ -499,11 +445,12 @@ fn analyze_value(
         }
         QT_STRING_VALUE => {
             let value_range_start = container_start + header.value_bit_field as usize;
-            if header.latin_or_int_value_flag {
-                analyze_latin1_string_value(data, value_range_start)
+            let analyze_string_value = if header.latin_or_int_value_flag {
+                analyze_latin1_string_value
             } else {
-                analyze_utf16_string_value(data, value_range_start)
-            }
+                analyze_utf16_string_value
+            };
+            analyze_string_value(data, value_range_start)
         }
         QT_ARRAY_VALUE => {
             let value_range_start = container_start + header.value_bit_field as usize;
@@ -513,9 +460,7 @@ fn analyze_value(
             let value_range_start = container_start + header.value_bit_field as usize;
             analyze_object(data, value_range_start)
         }
-        _ => Err(AnalysisError::DataAnalysisError(
-            data::Error::UnknownQtValue,
-        )),
+        _ => Err(AnalysisError::data(data::Error::UnknownQtValue)),
     }
 }
 
@@ -540,15 +485,13 @@ fn analyze_latin1_string_value(
     value_start: usize,
 ) -> Result<(data::Value, usize), AnalysisError> {
     let size_field_range = value_start..(value_start + metadata::LATIN1_SIZE_FIELD_LENGTH);
-    match data.get(size_field_range) {
-        Some(data) => {
-            let (bytefield, value_end) = analyze_latin1_string(data, value_start);
-            Ok((data::Value::Latin1String(bytefield), value_end))
-        }
-        None => Err(AnalysisError::DataAnalysisError(
-            data::Error::InvalidValueLength,
-        )),
-    }
+    let string_data = data
+        .get(size_field_range)
+        .ok_or(data::Error::InvalidValueLength)
+        .map_err(AnalysisError::data)?;
+
+    let (bytefield, value_end) = analyze_latin1_string(string_data, value_start);
+    Ok((data::Value::Latin1String(bytefield), value_end))
 }
 
 fn analyze_utf16_string_value(
@@ -556,13 +499,11 @@ fn analyze_utf16_string_value(
     value_start: usize,
 ) -> Result<(data::Value, usize), AnalysisError> {
     let size_field_range = value_start..(value_start + metadata::UTF16_SIZE_FIELD_LENGTH);
-    match data.get(size_field_range) {
-        Some(data) => {
-            let (bytefield, value_end) = analyze_utf16_string(data, value_start);
-            Ok((data::Value::Utf16String(bytefield), value_end))
-        }
-        None => Err(AnalysisError::DataAnalysisError(
-            data::Error::InvalidValueLength,
-        )),
-    }
+    let string_data = data
+        .get(size_field_range)
+        .ok_or(data::Error::InvalidValueLength)
+        .map_err(AnalysisError::data)?;
+
+    let (bytefield, value_end) = analyze_utf16_string(string_data, value_start);
+    Ok((data::Value::Utf16String(bytefield), value_end))
 }
